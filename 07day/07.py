1d5133582629075616f65690ee982abcbf03d678
Erick L <ecklpz@gmail.com>
2025-12-17 19:49:29 -0500

    aoc-2025-day-07: solved part 1

--- 07day/07.py
+++ 07day/07.py
@@ -1,26 +1,41 @@
 def parse_data(path_file):
     try:
         with open(path_file, "r") as f:
-            matrix = [list(line.strip()) for line in f.readlines()]
-        return matrix
+            return [line.strip() for line in f]
     except FileNotFoundError:
         print(f"[error] {path_file} not found")
         return None
 
-def check_splits(line):
-    # check by lines if the beans has its own col 
-    # if it does not, shouldn't to be count
-    # find the S to start
-    # goes downward until find splitter ^
-    # could use a num list position of col to go down
-    # avoid overflowing list
-    return False, None
+def check_splits(cols, line):
+    new_cols = set()
+    counter = 0
+    line_len = len(line)
+    display_line = list(line)
+
+    for col in cols:
+        if line[col] == "^":
+            counter += 1
+            if col - 1 >= 0:
+                new_cols.add(col - 1)
+                display_line[col - 1] = "|"
+            if col + 1 < line_len:
+                new_cols.add(col + 1)
+                display_line[col + 1] = "|"
+        else:
+            new_cols.add(col)
+            display_line[col] = "|"
+
+    print("".join(display_line))
+    return new_cols, counter
 
 def init_simulation(matrix):
-    ans = 0
+    current_cols = {matrix[0].index("S")}
+    total_splits = 0
     for line in matrix:
-        was_splited, last_cols = check_splits(line)
-    return ans
+        current_cols, counter = check_splits(current_cols, line)
+        total_splits += counter
+    print(f"\nTotal splits: {total_splits}")
+    return total_splits
 
 FILE_PATH = "07.txt"
 matrix = parse_data(FILE_PATH)
